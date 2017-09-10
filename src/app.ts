a07196bf2b9fe891cfa8cde840d9ae9a2c0eff20
euky <riverage@163.com>
2017-09-10 20:29:43 +0800

    update logic

--- src/app.ts
+++ src/app.ts
@@ -16,41 +16,40 @@ app.post('/upload', upload.single('icon'), (req, res) => {
 
     const buffer = sharp(req.file.buffer)
 
-    const output = fs.createWriteStream(path.resolve(__dirname, '../data/example.zip'))
-    const archive = archiver('zip', { zlib: { level: 9 } })
-    archive.on('warning', (err) => {
-        if (err.code === 'ENOENT') {
-            console.log('waringing...')
-        } else {
-            console.log('error')
-        }
-    })
-    archive.on('error', (err) => {
-        console.log('error')
-    })
-    archive.pipe(output)
-    archive.append(fs.createReadStream(path.resolve(__dirname, '../assets/Contents.json')), {name: 'Contents.json'})
+    const outputFile = path.resolve(__dirname, `../data/${crypto.randomBytes(16).toString('hex')}`)
+    const output = fs.createWriteStream(outputFile)
+    const archive = archiver('zip', { gzip: true })
 
-    const map = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../assets/size-map.json'), 'utf-8')).ios as {[props: string]: string[]}
-    const keys = Object.keys(map)
-    const p = keys.map((e) => {
-        const [d1, d2] = e.split('x')
+    output.on('close', () => {
+        res.setHeader('Content-Disposition', 'attachment; filename=icon.zip')
+        res.sendFile(outputFile)
+    })
+
+
+    archive.pipe(output)
+    archive.append(fs.createReadStream(path.resolve(__dirname, '../assets/Contents.json')), { name: 'Contents.json', prefix: 'ios' })
+
+    const sizeMap = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../assets/size-map.json'), 'utf-8')) as { dim: string, files?: string[], dir?: string }[]
+
+
+    const buffers = sizeMap.map((e) => {
+        const [d1, d2] = e.dim.split('x')
 
         return buffer.resize(parseInt(d1, 10), parseInt(d2, 10))
             .toBuffer()
             .then((b) => {
-                const fileNames = map[e]
-                fileNames.map((f) => archive.append(b, { name: f }))
+                if (e.files) {
+                    e.files.map((f) => archive.append(b, { name: f, prefix: 'ios' }))
+                }
+                if (e.dir) {
+                    archive.append(b, { name: 'ic_launcher.png', prefix: 'android/' + e.dir })
+                }
+
             })
     })
 
-    Promise.all(p).then((resolve) => {
-        archive.finalize()
-        // res.sendFile(path.resolve(__dirname, '../data/example.zip'))
-    })
+    Promise.all(buffers).then(() => archive.finalize())
 
-    res.send('OK')
-     
 })
 
 app.listen(3000, () => console.log('start....'))
